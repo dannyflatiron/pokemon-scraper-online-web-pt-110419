1f3e7511ec54ef822cae0e9426d3f23fed42c6e6
Daniel Reina <dannyflatiron@gmail.com>
2020-01-30 04:29:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,19 +10,18 @@ attr_accessor :id, :name, :type, :db
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
-  #  sql = <<-SQL
-  #    INSERT INTO pokemon (name, type, db)
-  #    VALUES (?, ?, ?)
-  #  SQL
-   #
-  #  DB[:conn].execute(sql, self.name, self.type, self.db)
-  #  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+   sql = <<-SQL
+     INSERT INTO pokemon (name, type, db)
+     VALUES (?, ?, ?)
+   SQL
+
+   DB[:conn].execute(sql, self.name, self.type, self.db)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
   def self.find(id, db)
     # find pokemon by ID
     # return a new pokemon object
-
+    db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
   end
 end
